2b2aa8ed6e2f1c9acd9d78e24507941b25d12734
Srikalyan Megati <kalmegati@gmail.com>
2019-08-01 19:48:16 +0000

    Automatically backed up by Learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -9,7 +9,7 @@ def fourth_challenge
         {name: "Mercutio", age: "18", attitude: "hot-headed"}
       ]
    }, 
-   :capulet => {
+   :capulet => 
       :patriarch => {name: "Lord Capulet", age: "50"},
       :matriarch => {name: "Lady Capulet", age: "51"},
       :heroine => {name: "Juliet", age: "15", status: "alive"},
